4c3fdcfbcafde61b29fd5a0b0f68ef2190dfa144
stiven45ruiz <stiven45ruiz@gmail.com>
2020-05-09 00:37:51 -0500

    Añadiendo mejoras

--- Ejercicios/ejercicio5.js
+++ Ejercicios/ejercicio5.js
@@ -1,4 +1,3 @@
-const peso_planate = document.getElementById("peso_planeta");
 const g_tierra = 9.8;
 const g_marte = 3.711;
 const g_jupiter = 24.8;
@@ -9,32 +8,12 @@ const g_saturno = 10.44;
 const g_urano = 11.15;
 const g_sol = 274;
 
-var usuario = prompt("Cual es tu peso?");
-var planeta = parseInt(prompt("Elige tu planeta\n1 es marte, 2 es jupiter"));
-var peso = parseInt(usuario);
+let peso_usuario = document.getElementById("peso_usuario").value
+
 
 var peso_final;
 var nombre;
-if (planeta == 1)
-{
-  peso_final = peso * g_marte / g_tierra;
-  nombre ="Marte";
-}
-else if (planeta == 2)
-{
-  peso_final = peso * g_jupiter / g_tierra;
-  nombre ="Jupiter";
-}
-else if (planeta == 2)
-{
-  peso_final = peso * g_jupiter / g_tierra;
-  nombre ="mercurio";
-}
-else
-{
-peso_final = 1000000; 
-nombre = "Kripton";
-}
+
 
 
 peso_final = parseInt(peso_final);
