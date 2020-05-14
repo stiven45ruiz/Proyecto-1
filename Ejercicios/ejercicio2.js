6d5092b4370b721d537bd586568a616d7c77fbf9
stiven45ruiz <stiven45ruiz@gmail.com>
2020-05-13 21:50:06 -0500

    Bugs

--- Ejercicios/ejercicio2.js
+++ Ejercicios/ejercicio2.js
@@ -1,27 +1,38 @@
-var sm = document.getElementById("accion");
-var rs = document.getElementById("respuesta")
-var n1 = parseInt(document.getElementById("numero1").value ) ;
-var n2 = parseInt(document.getElementById("numero2").value ) ;
-
 
 function suma ()
 {
+    let sm = document.getElementById("accion");
+    let rs = document.getElementById("respuesta")
+    let n1 = parseInt(document.getElementById("numero1").value ) ;
+    let n2 = parseInt(document.getElementById("numero2").value ) ;
    sm.innerHTML = ("+");
    rs.innerHTML = (n1 + n2) 
 }    
 function resta ()
 {
+    let sm = document.getElementById("accion");
+    let rs = document.getElementById("respuesta")
+    let n1 = parseInt(document.getElementById("numero1").value ) ;
+    let n2 = parseInt(document.getElementById("numero2").value ) ;
    sm.innerHTML = ("-");
    rs.innerHTML = (n1 - n2)
 } 
 function multiplica()
 {
-   sm.innerHTML = ("*");
+    let sm = document.getElementById("accion");
+    let rs = document.getElementById("respuesta")
+    let n1 = parseInt(document.getElementById("numero1").value ) ;
+    let n2 = parseInt(document.getElementById("numero2").value ) ;
+    sm.innerHTML = ("*");
    rs.innerHTML = (n1 * n2)
 } 
 function divide ()
 {
-   sm.innerHTML = ("/");
+    let sm = document.getElementById("accion");
+    let rs = document.getElementById("respuesta")
+    let n1 = parseInt(document.getElementById("numero1").value ) ;
+    let n2 = parseInt(document.getElementById("numero2").value ) ;
+    sm.innerHTML = ("/");
    rs.innerHTML = (n1 / n2)
 } 
 
